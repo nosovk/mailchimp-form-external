193616a15984285699dace5e09ba9ff3c426fc9c
neramik <neramik@evergreens.in.ua>
2020-11-01 21:55:51 +0200

    updated react app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import isEmail from 'isemail';
 import Loader from './Loader';
+import Select from './Select';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+  
 
   const initial = {
     email: '',
@@ -35,30 +40,20 @@ function App() {
         ...state,
         [field]: 'Поле не должно быть пустым!'
       }));
-    } else if (field === 'email') { 
+    } else if (field === 'email') {
       if (!isEmail.validate(e.target.value)) {
         setErrors((state) => ({
           ...state,
           [field]: 'Введите правильный email'
-        }));  
+        }));
       } else {
         setErrors((state) => ({
           ...state,
           [field]: ''
-        }));  
+        }));
       }
 
-    } else if(field === 'tag') {
-      const name = (tags.find((tag) => tag.id === e.target.value)).name;
-
-      setSelectedTags((tags) => ([
-        ...tags,
-        name
-      ]));
-
-      setTagsToShow(tagsToShow.filter((item) => item.id !== e.target.value));
-    
-    } else {
+    }  else {
       setErrors((state) => ({
         ...state,
         [field]: ''
@@ -66,30 +61,57 @@ function App() {
     }
   };
 
+  const notifySuccess = () => {
+      toast.success('Пользователь добавлен в базу!', {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    setState(initial);
+    setSelectedTags([]);
+    setTagsToShow(tags);
+  };
+
+  const notifyError = () => toast.error('Пользователь не добавлен в базу!', {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+
   const submit = (e) => {
     e.preventDefault();
     const bodyToSend = {
       audience: state.audience,
-      tags: selectedTags,
+      tags: selectedTags.map((option) => option.name),
       contact: {
-          email: state.email,
-          organization: state.organization,
-          fullname: state.fullname,
-          address: state.address,
-          phone: state.phone
+        email: state.email,
+        organization: state.organization,
+        fullname: state.fullname,
+        address: state.address,
+        phone: state.phone
       }
     }
-    fetch('/api/addContact', {
+    fetch('http://localhost:4000/addContact', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify(bodyToSend)
     }).then((resp) => {
       if (resp.ok) {
-        window.location.reload()
+        notifySuccess();
       } else {
         setError('Такой email уже существует!');
+        notifyError();
       }
     }).catch((e) => console.log(e));
   };
@@ -97,7 +119,7 @@ function App() {
   useEffect(() => {
     const fetchAudiences = async () => {
       setLoading(true);
-      const resp = await fetch('/api/audiences');
+      const resp = await fetch('http://localhost:4000/audiences');
       const body = await resp.json();
       setAudiences(body);
       setState((state) => ({
@@ -113,7 +135,7 @@ function App() {
     if (state.audience === '') return;
     const fetchTags = async () => {
       setLoading(true);
-      const resp = await fetch(`/api/audiences/${state.audience}/tags`);
+      const resp = await fetch(`http://localhost:4000/audiences/${state.audience}/tags`);
       const body = await resp.json();
       setTags(body);
       setTagsToShow(body);
@@ -123,108 +145,134 @@ function App() {
   }, [ state.audience ]);
 
 
+  const onOptionSelected = (option) => {
+    setSelectedTags((tags) => ([
+      ...tags,
+      option
+    ]));
+    setTagsToShow((tags) => {
+      return tags.filter((item) => item.id !== option.id);
+    });
+  };
+
+  const onTagDeleted = (option) => {
+    setSelectedTags((tags) => {
+        return tags.filter((tag) => tag.id !== option.id)
+    });
+    setTagsToShow(() => {
+      return tags.filter((tag) => {
+        const candidateTag = selectedTags.find((item) => item.id == tag.id && item.id !== option.id);
+        return !candidateTag;
+      })
+    });
+
+  };
+
   return (
-    <div className="App">
-      <div className="container">
-        <h1>РЕГИСТРАЦИОННАЯ ФОРМА ДЛЯ РАССЫЛКИ НОВОСТЕЙ И ПРАЙСОВ</h1>
-        <div className="form-container">
-          <form onSubmit={submit}>
-            <p>
+      <div className="App">
+        <div className="container">
+          <h1>РЕГИСТРАЦИОННАЯ ФОРМА ДЛЯ РАССЫЛКИ НОВОСТЕЙ И ПРАЙСОВ</h1>
+          <div className="form-container">
+            <form onSubmit={submit}>
+              <p>
                 Уважаемые Партнеры,<br />
                 убедительно просим Вас внимательно отнестись к заполнению этой формы.<br />
-                От этого будет зависеть оперативность, качество и своевременность 
+                От этого будет зависеть оперативность, качество и своевременность
                 информирования Вас о всех новинках и прайс-листах нашей компании.<br />
                 Благодарим Вас!
-            </p>
-            <p style={{'color': 'red'}}>{error}</p>
-            <div className="fields">
-              <div className="form-control">
-                <label>Email</label>
-                <input type="email" required value={state.email} onChange={onChange('email')}  />
-                <small className={`${errors.email ? 'errors' : ''}`}>{errors.email}</small>
-              </div>
-              <div className="form-control">
-                <label>ФИО</label>
-                <input type="text" value={state.fullname} required onChange={onChange('fullname')} />
-                <small className={`${errors.fullname ? 'errors' : ''}`}>{errors.fullname}</small>
-              </div>
-              <div className="form-control">
-                <label>Номер телефона</label>
-                <input type="phone" value={state.phone} required onChange={onChange('phone')} />
-                <small className={`${errors.phone ? 'errors' : ''}`}>{errors.phone}</small>
-              </div>
-              <div className="form-control">
-                <label>Адрес</label>
-                <input type="text" value={state.address} required onChange={onChange('address')} />
-                <small className={`${errors.address ? 'errors' : ''}`}>{errors.address}</small>
-              </div>
-              <div className="form-control">
-                <label>Организация</label>
-                <input type="text" value={state.organization} required onChange={onChange('organization')} />
-                <small className={`${errors.organization ? 'errors' : ''}`}>{errors.organization}</small>
-              </div>
-              {
-                !audiences && isLoading 
-                ? <Loader />
-                : (
-                  <div className="form-control">
-                    <label>Аудитории</label>
-                    <select value={state.audience} onChange={onChange('audience')}>
-                      {
-                        audiences 
-                        ? audiences.map((item) => (
-                          <option value={item.id} key={item.id}>
-                            {item.name}
-                          </option>
-                        )) 
-                        : null
-                      }
+              </p>
+              <p style={{'color': 'red'}}>{error}</p>
+              <div className="fields">
+                <div className="form-control">
+                  <label>Email</label>
+                  <input type="email" required value={state.email} onChange={onChange('email')}  />
+                  <small className={`${errors.email ? 'errors' : ''}`}>{errors.email}</small>
+                </div>
+                <div className="form-control">
+                  <label>ФИО</label>
+                  <input type="text" value={state.fullname} required onChange={onChange('fullname')} />
+                  <small className={`${errors.fullname ? 'errors' : ''}`}>{errors.fullname}</small>
+                </div>
+                <div className="form-control">
+                  <label>Номер телефона</label>
+                  <input type="phone" value={state.phone} required onChange={onChange('phone')} />
+                  <small className={`${errors.phone ? 'errors' : ''}`}>{errors.phone}</small>
+                </div>
+                <div className="form-control">
+                  <label>Адрес</label>
+                  <input type="text" value={state.address} required onChange={onChange('address')} />
+                  <small className={`${errors.address ? 'errors' : ''}`}>{errors.address}</small>
+                </div>
+                <div className="form-control">
+                  <label>Организация</label>
+                  <input type="text" value={state.organization} required onChange={onChange('organization')} />
+                  <small className={`${errors.organization ? 'errors' : ''}`}>{errors.organization}</small>
+                </div>
+                {
+                  !audiences && isLoading
+                      ? <Loader />
+                      : (
+                          <div className="form-control">
+                            <label>Аудитории</label>
+                            <select value={state.audience} onChange={onChange('audience')}>
+                              {
+                                audiences
+                                    ? audiences.map((item) => (
+                                        <option value={item.id} key={item.id}>
+                                          {item.name}
+                                        </option>
+                                    ))
+                                    : null
+                              }
 
-                    </select>
-                  </div>
-                )            
-              }
-              {
-                state.audience && isLoading 
-                ? <Loader />
-                : tags ? (
-                  <div className="form-control multiple-select">
-                    <label>Тэги</label>
-                    <span className="multiple-selection">{selectedTags.join(', ')}</span>
-                    <select value={state.tag} onChange={onChange('tag')}>
-                      <option value="" >
-                            {" "}
-                      </option>
-                      {
-                        tagsToShow 
-                        ? tagsToShow.map((item) => (
-                          <option value={item.id} key={item.id}>
-                            {item.name}
-                          </option>
-                        )) 
-                        : null
-                      }
-                    </select>
-                  </div>
-                )    
-                : null        
-              }            
-              <div className="submit_container clear">
-                <button type="submit" className="submit">Подписаться</button>
+                            </select>
+                          </div>
+                      )
+                }
+                {
+                  state.audience && isLoading
+                      ? <Loader />
+                      : tags ? (
+                          <div className="form-control">
+                            <label>Тэги</label>
+                            <Select 
+                              options={tagsToShow}
+                              onOptionSelected={onOptionSelected}
+                              selectedOptions={selectedTags}
+                              onTagDeleted={onTagDeleted}
+                            />
+                          </div>
+                      )
+                      : null
+                } 
+
+                <div className="submit_container clear">
+                  <button type="submit" className="submit">Подписаться</button>
+                </div>
               </div>
-            </div>
-          </form>
-        </div>
-        <div className="poweredWrapper">
+            </form>
+          </div>
+          <div className="poweredWrapper">
           <span className="poweredBy">
             <a href="http://www.mailchimp.com/email-referral/?utm_source=freemium_newsletter&amp;utm_medium=email&amp;utm_campaign=referral_marketing&amp;aid=2a09abbf5a8dc4dfa4874899c&amp;afl=1">
               <img src="https://cdn-images.mailchimp.com/monkey_rewards/MC_MonkeyReward_15.png" border="0" alt="Email Marketing Powered by Mailchimp" title="Mailchimp Email Marketing" width="139" height="54" />
             </a>
           </span>
-        </div>
+          </div>
 
+        </div>
+        <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+        />
       </div>
-    </div>
   );
 }
 
